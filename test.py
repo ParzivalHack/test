1ca81fb79d1ca033312bb46d2a98f7ec86b7dcc4
Tommaso Bona <piergeolo@gmail.com>
2023-03-22 09:58:05 +0100

    Update test.py

--- test.py
+++ test.py
@@ -6,13 +6,23 @@ from colorama import init, Fore, Style
 
 init()  # initialize colorama
 
-web3 = Web3(Web3.HTTPProvider("https://mainnet.infura.io/v3/97d9b42d4a4a4c0ca98ea173bdcf4250"))
+w3 = Web3(Web3.HTTPProvider('https://mainnet.infura.io/v3/97d9b42d4a4a4c0ca98ea173bdcf4250'))
 print("Connected to Web3: ", web3.isConnected())
 
-to_account = str(input("Import your wallet: "))
-balance = web3.eth.get_balance(to_account)
-print("Your current balance is 0.0", balance, "ETH.")
-print("Mining wallets...")
+# Ask the user for their wallet address
+wallet_address = input("Enter your wallet address: ")
+
+# Check if the wallet address is valid
+if not w3.isAddress(wallet_address):
+    print("Invalid wallet address")
+else:
+    # Retrieve the balance of the wallet address in ETH
+    balance_wei = w3.eth.getBalance(wallet_address)
+    balance_eth = w3.fromWei(balance_wei, 'ether')
+    
+    # Print the balance of the wallet address
+    print("Your current balance is", balance_eth, "ETH")
+    print("Mining wallets...")
 while True:
     # Print the hash in red
     print(Fore.RED + ''.join(random.choice(string.ascii_letters + string.digits) for i in range(50)) + '--0.0000 ETH')
